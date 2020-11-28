b9c48e5211b20c47d7d878b409776afa86d65ffe
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2020-11-28 04:17:36 -0700

    Added Styling
    Styled All Major Components of Website

--- Front-End/React Front-End/src/Containers/App.js
+++ Front-End/React Front-End/src/Containers/App.js
@@ -1,5 +1,11 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
+import catalog from '../resources/images/bracelts.jpg';
+import stand from '../resources/images/stand.jpg'
+
+import etsy from '../resources/images/social-media/etsy.png';
+import facebook from '../resources/images/social-media/facebook.png';
+import instagram from '../resources/images/social-media/instagram.png';
 
 
 class App extends PureComponent {
@@ -8,30 +14,103 @@ class App extends PureComponent {
   }
 
   render () {
+    let catalogItem =[
+      {id:0, link:"", descrption:""},
+      {id:1, link:"", descrption:""},
+      {id:2, link:"", descrption:""},
+      {id:3, link:"", descrption:""},
+      {id:4, link:"", descrption:""},
+      {id:5, link:"", descrption:""},
+      {id:6, link:"", descrption:""},
+      {id:7, link:"", descrption:""}
+    ];
+
+    let entireCatalog = catalogItem.map((item) =>{
+      return (<li key = {item.id} style={{backgroundImage: `url(${catalog})`}}></li>);
+    })
+
+
+    let events =[
+      {id:0, pic:"", location:"Parker Local Store", date:"Now", link:"https://www.google.com/maps/dir//the+LOCAL+Parker,+19565+Mainstreet,+Parker,+CO+80138/@39.5185752,-104.7620033,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x876c91fb146edb0f:0x31e54245863d15c4!2m2!1d-104.7620033!2d39.5185752!3e0"},
+      {id:1, pic:"", location:"Parker Local Store", date:"Now", link:"https://www.google.com/maps/dir//the+LOCAL+Parker,+19565+Mainstreet,+Parker,+CO+80138/@39.5185752,-104.7620033,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x876c91fb146edb0f:0x31e54245863d15c4!2m2!1d-104.7620033!2d39.5185752!3e0"},
+      {id:2, pic:"", location:"Parker Local Store", date:"Now", link:"https://www.google.com/maps/dir//the+LOCAL+Parker,+19565+Mainstreet,+Parker,+CO+80138/@39.5185752,-104.7620033,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x876c91fb146edb0f:0x31e54245863d15c4!2m2!1d-104.7620033!2d39.5185752!3e0"},
+      {id:3, pic:"", location:"Parker Local Store", date:"Now", link:"https://www.google.com/maps/dir//the+LOCAL+Parker,+19565+Mainstreet,+Parker,+CO+80138/@39.5185752,-104.7620033,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x876c91fb146edb0f:0x31e54245863d15c4!2m2!1d-104.7620033!2d39.5185752!3e0"},
+      {id:4, pic:"", location:"Parker Local Store", date:"Now", link:"https://www.google.com/maps/dir//the+LOCAL+Parker,+19565+Mainstreet,+Parker,+CO+80138/@39.5185752,-104.7620033,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x876c91fb146edb0f:0x31e54245863d15c4!2m2!1d-104.7620033!2d39.5185752!3e0"}
+    ];
+    let allEvents = events.map((event) =>{
+      return(
+      <li className="event"key={event.id}>
+        <a href={event.link} target="_blank"><img src={stand} alt="eventLocation"/></a>
+        <p>
+          Where: <a href={event.link} target="_blank">{event.location}</a>
+        </p>
+        
+        <p>
+          When: {event.date}
+        </p>
+      </li>);
+    })
+
+
     return (
-      <Fragment>
-        <nav class="navBar">
-            <ul className="navList">
-                <li><a class="button" href="">Shop</a></li>
-                <li className=""><a class="button" href="">Locations</a></li>
-                <li className=""><a class="button" href="">About Me</a></li>
-                <li className=""><a class="button" href="">Contact Us</a></li>
+      <div id="main-container">
+        <nav className="navBar">
+            <ul className="navList socialMedia">
+                <li><a href={"https://www.instagram.com/lindibracelets/"} target="_blank"><img className="navSocialMediaIcon"src={instagram}/></a></li>
+                <li><a href={"https://www.facebook.com/LinDiBracelets/"} target="_blank"><img className="navSocialMediaIcon"src={facebook}/></a></li>
+                <li><a href={"https://www.etsy.com/shop/LinDiBracelets?ref=simple-shop-header-name&listing_id=584303931"} target="_blank"><img className="navSocialMediaIcon"src={etsy}/></a></li>
+         
+
+                
+                
+                
+                <li><a href="./">Contact Us</a></li>
+                <li><a href="./">About Me</a></li>
+                <li><a href="./">Events</a></li>
+                <li><a href="./">Shop</a></li>
             </ul>
-        </nav>
+        </nav>        
       
         
         <div className="headerText nomobile">
-            <h1 className ="headerBold headerText">Love</h1>
-            <h1 className ="headerReg headerText">what you wear. be</h1>
-            <h1 className ="headerBold headerText">Unique</h1>
+            <h1 className ="lindi"> Lindi Bracelts</h1>
+            <h1 className ="headerBold">Love</h1>
+            <h1 className ="headerReg">what you wear. be</h1>
+            <h1 className ="headerBold">Unique</h1>
         </div>
         
         <div className="shop">
           <ul>
-            <li>Item</li>
+           {entireCatalog}
+          </ul> 
+        </div>
+
+        <div className="eventsTitle">
+          <h1>Events</h1>
+          <ul>
+            {allEvents}
           </ul>
         </div>
-      </Fragment>
+
+        <div className="contactMe">
+          <h1>Contact Me</h1>
+          <form className="contactMeForm">
+            <input type="text" Placeholder="Subject"></input>
+            <input type="text" Placeholder="Email"></input>
+            <textarea  className="formContent"></textarea >
+            <button className="button" type="submit">Submit</button>
+          </form>
+        </div>
+
+        <div>
+          <ul className="socialMedia">
+            <li><a href={"https://www.instagram.com/lindibracelets/"} target="_blank"><img className="socialMediaIcon"src={instagram}/></a></li>
+            <li><a href={"https://www.facebook.com/LinDiBracelets/"} target="_blank"><img className="socialMediaIcon"src={facebook}/></a></li>
+            <li><a href={"https://www.etsy.com/shop/LinDiBracelets?ref=simple-shop-header-name&listing_id=584303931"} target="_blank"><img className="socialMediaIcon"src={etsy}/></a></li>
+          </ul>
+        </div>
+
+      </div>
     );
   }
 }
