fa97284b4d29d112a69fc709c676c06d160f12b1
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2020-12-16 02:22:35 -0700

    Tweaks

--- Front-End/src/Containers/App.js
+++ Front-End/src/Containers/App.js
@@ -14,9 +14,11 @@ import facebook from '../resources/images/social-media/facebook.png';
 import instagram from '../resources/images/social-media/instagram.png';
 
 class App extends PureComponent {
+  
   constructor(props)
   {
     super(props)
+    this.mainC = React.createRef();
     this.Shop = React.createRef();
     this.Gallery = React.createRef();
     this.Events = React.createRef();
@@ -81,20 +83,16 @@ class App extends PureComponent {
   }
 
   inViewPort = (ref) =>{
+   let el = ref.current;
+   let percentVisible = 40;
+    let
+    rect = el.getBoundingClientRect(),
+    windowHeight = (window.innerHeight || document.documentElement.clientHeight);
 
-    // Get it's position in the viewport
-    var bounding = ref.current.getBoundingClientRect();
-
-    if (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+  return !(
+    Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < percentVisible ||
+    Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+  )
   }
 
   
@@ -103,16 +101,7 @@ class App extends PureComponent {
   render () {
     
 
-    let catalogItem =[
-      {id:0, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:1, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:2, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:3, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:4, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:5, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:6, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""},
-      {id:7, link:"https://www.etsy.com/listing/714558154/crochet-pattern-pdf-crochet-beaded?ref=shop_home_active_12", descrption:""}
-    ];
+ 
     let entireCatalog = this.state.shop.map((item) =>{
       return (
       <li key = {item.id} style={{backgroundImage: `url(${item.link})`}}>
@@ -125,29 +114,6 @@ class App extends PureComponent {
       </li>);
     })
 
-    let galleryItem =[
-      {id:0, link:"", descrption:""},
-      {id:1, link:"", descrption:""},
-      {id:2, link:"", descrption:""},
-      {id:3, link:"", descrption:""},
-      {id:4, link:"", descrption:""},
-      {id:5, link:"", descrption:""},
-      {id:6, link:"", descrption:""},
-      {id:7, link:"", descrption:""},
-      {id:8, link:"", descrption:""},
-      {id:9, link:"", descrption:""},
-      {id:10, link:"", descrption:""},
-      {id:11, link:"", descrption:""},
-      {id:12, link:"", descrption:""},
-      {id:13, link:"", descrption:""},
-      {id:14, link:"", descrption:""},
-      {id:15, link:"", descrption:""},
-      {id:16, link:"", descrption:""},
-      {id:17, link:"", descrption:""},
-      {id:18, link:"", descrption:""},
-      {id:19, link:"", descrption:""},
-      {id:20, link:"", descrption:""},
-    ];
     let entireGallery = this.state.gallery.map((item)=>{
       return(
       <li key={item.id}>
@@ -159,22 +125,16 @@ class App extends PureComponent {
 
 
     let events =[
-      {id:0, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:1, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:2, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:3, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:4, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:5, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:6, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:7, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:8, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
-      {id:9, pic:"", location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
+      {id:0, pic:'https://secureservercdn.net/198.71.233.184/49c.223.myftpupload.com/wp-content/uploads/2015/09/06.22.2017_AuroraLifestyle_192-1024x682.jpg', location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
+      {id:1, pic:stand, location:"The Local Parker,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
+      {id:2, pic:'//geo1.ggpht.com/cbk?panoid=VUbq-xRlkLkqNJk226wTxQ&output=thumbnail&cb_client=search.gws-prod.gps&thumb=2&yaw=47.1674&pitch=0&thumbfov=100&w=286&h=192', location:"Salon,CO", date:"Now", link:"https://www.google.com/maps/place/the+LOCAL+Parker/@39.5185752,-104.7620033,17z/data=!3m1!4b1!4m5!3m4!1s0x876c91fb146edb0f:0x31e54245863d15c4!8m2!3d39.5185752!4d-104.7620033"},
+     
       
     ];
     let allEvents = events.map((event) =>{
       return(
       <li className="event"key={event.id}>
-        <a href={event.link} target="_blank" rel="noreferrer"><img src={stand} alt="eventLocation"/></a>
+        <a href={event.link} target="_blank" rel="noreferrer"><img src={event.pic} alt="eventLocation"/></a>
         <div className="eventDescription">
           <p>
             Where: <a href={event.link} target="_blank" rel="noreferrer">{event.location}</a>
@@ -189,7 +149,7 @@ class App extends PureComponent {
 
 
     return (
-      <div id="main-container" onScroll={this.checkSelected}>
+      <div ref={this.mainC} id="main-container" onScroll={this.checkSelected}>
         <nav className="navBar">
             <ul className="navList">
                 <li><a href={"https://www.instagram.com/lindibracelets/"} target="_blank" rel="noreferrer"><img className="navSocialMediaIcon"src={instagram} alt="InstagramLogo"/></a></li>
@@ -213,43 +173,44 @@ class App extends PureComponent {
             <h2 className ="headerBold">Unique</h2>
         </div>
         
-        <div className="shop">
-          <h1 ref={this.Shop}>Shop</h1>
+        <div ref={this.Shop} className="shop">
+          <h1 >Shop</h1>
           <ul>
            {entireCatalog}
           </ul> 
         </div>
 
-        <div className="gallery">
-          <h1 ref={this.Gallery}>Gallery</h1>
+        <div ref={this.Gallery} className="gallery">
+          <h1>Gallery</h1>
           <ul>
             {entireGallery}
           </ul>
         </div>
 
-        <div className="eventsTitle">
-          <h1 ref={this.Events}>Events</h1>
+        <div ref={this.Events} className="eventsTitle">
+          <h1>Events</h1>
           <ul>
             {allEvents}
           </ul>
         </div>
 
-        <div  className="aboutMe">
-          <h1 ref={this.AboutMe}> About Me</h1>
-          <img src={author} alt="Author"/>
-          <p>I started croqueing a long time ago. It is my passion
-            it is the best thing in the world. Filler text to put here
-            Filler text to put here Filler text to put here Filler text to put here 
-            Filler text to put here Filler text to put here Filler text to put here 
-            Filler text to put here Filler text to put here Filler text to put here 
-            Filler text to put here Filler text to put here Filler text to put here 
-            Filler text to put here Filler text to put here Filler text to put here 
-            Filler text to put here Filler text to put here Filler text to put here 
+        <div ref={this.AboutMe} className="aboutMe">
+          <h1> About Me</h1>
+          <img src={'https://i.etsystatic.com/isa/d8132b/1911746164/isa_760xN.1911746164_8nx5.jpg?version=0'} alt="Author"/>
+          <p>I learned how to crochet from my mom and grandma when I was little. The new high quality thin cotton strings in variety of colors in combination with the beads created new possiblity in front of me. I started making the bracelets in 2017.
+          </p>
+
+          <p>
+          I love experimenting and incorporating different patterns with the sparkle of the beads and jam stones.
+
+Some bracelets are inspired by the ancient Bulgarian embroidery patterns, and others are sparkling asymmetrically or challenging the color combinations.
+
+Every bracelet I create is unique.
           </p>
         </div>
 
-        <div className="contactMe">
-          <h1 ref={this.ContactMe}>Contact Me</h1>
+        <div ref={this.ContactMe} className="contactMe">
+          <h1>Contact Me</h1>
           <p>Got something different in mind? Send me an email and we can make it a realty</p>
           <form className="contactMeForm">
             <input type="text" placeholder="Email"></input>
