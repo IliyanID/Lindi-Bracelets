ecadc91430fd46bd63aceee76af8e0d14545252c
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2021-09-19 01:33:08 -0600

    Added delay

--- Back-End/src/main/java/com/tco/misc/response/Etsy.java
+++ Back-End/src/main/java/com/tco/misc/response/Etsy.java
@@ -36,6 +36,7 @@ public class Etsy extends Response{
 
     private String getItemCoverPhoto(int id,String APIKey){
         try{
+            Thread.sleep(500);
             String requestURL = "https://openapi.etsy.com/v2/listings/" + id + "/images?api_key=" + APIKey;
             JSONObject coverPhotoObj= new Request(requestURL).getArray("results").getJSONObject(0);
             return coverPhotoObj.getString("url_fullxfull");
